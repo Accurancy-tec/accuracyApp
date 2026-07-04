e302be195abe346a28323b8ab2930887273a01f0
MuriloGoms <murilogcordeiro08@gmail.com>
2026-07-04 17:00:32 -0300

    feat: Agora o login funciona, se o usuario ja estiver cadastro no banco, efetua o login

--- app/src/main/java/com/example/accurancymobileapp/api/ApiService.java
+++ app/src/main/java/com/example/accurancymobileapp/api/ApiService.java
@@ -1,14 +1,19 @@
 package com.example.accurancymobileapp.api;
 
+import com.example.accurancymobileapp.model.LoginResponse;
 import com.example.accurancymobileapp.model.User;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface ApiService {
 
     @POST("registerNewUser.php")
     Call<ResponseBody> registerNewUser(@Body User user);
+
+    @POST("login.php")
+    Call<LoginResponse> loginVerification(@Body User user);
 }
