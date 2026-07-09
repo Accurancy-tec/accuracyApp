903e2cd908cf2771cddd55902756536a14e1ab2c
MuriloGoms <murilogcordeiro08@gmail.com>
2026-07-09 17:13:20 -0300

    fix: Atualizei o endpoint de acordo com a documentação da brapi pra melhorar a escalabiliade no futuro

--- app/src/main/java/com/example/accurancymobileapp/network/service/BrapiService.java
+++ app/src/main/java/com/example/accurancymobileapp/network/service/BrapiService.java
@@ -5,10 +5,11 @@ import com.example.accurancymobileapp.response.QuoteResponse;
 import retrofit2.http.GET;
 import retrofit2.Call;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface BrapiService {
-    @GET("quote/{ticker}")
+    @GET("v2/stocks/quote")
     Call<QuoteResponse> getQuote(
-            @Path("ticker") String ticker
+            @Query("symbols") String symbols
     );
 }
