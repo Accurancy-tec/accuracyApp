86acb29eb7b77209a3b8acb309d86e2f9f180eb9
MuriloGoms <murilogcordeiro08@gmail.com>
2026-07-11 22:01:45 -0300

    feat: Adicionei um menu horizontal pra facilitar a navegação entre telas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.material)
     implementation("com.squareup.retrofit2:retrofit:3.0.0")
     implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.vico.compose)
     implementation(libs.vico.compose.m2)
     implementation(libs.vico.compose.m3)
