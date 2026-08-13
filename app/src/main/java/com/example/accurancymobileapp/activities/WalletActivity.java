ef00712b29607d14de77843365542a8b0d66952a
KlebersonDuarte <klebersonduartesantos39@gmail.com>
2026-08-13 20:27:11 -0300

    feat: finalizando o fragment da wallet

--- app/src/main/java/com/example/accurancymobileapp/activities/WalletActivity.java
+++ app/src/main/java/com/example/accurancymobileapp/activities/WalletActivity.java
@@ -1,117 +1,61 @@
 package com.example.accurancymobileapp.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.compose.ui.platform.ComposeView;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.fragment.app.Fragment;
 
 import com.example.accurancymobileapp.R;
-import com.example.accurancymobileapp.adapter.QuoteAdapter;
-import com.example.accurancymobileapp.model.QuoteData;
-import com.example.accurancymobileapp.model.QuoteResult;
-import com.example.accurancymobileapp.network.client.RetrofitClient;
-import com.example.accurancymobileapp.network.service.ApiService;
-import com.example.accurancymobileapp.response.QuoteResponse;
-import com.example.accurancymobileapp.ui.ChartHelper;
+import com.example.accurancymobileapp.fragments.AportesFragment;
+import com.example.accurancymobileapp.fragments.DashboardFragment;
+import com.example.accurancymobileapp.fragments.WalletFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class WalletActivity extends AppCompatActivity {
-
-    RecyclerView recyclerInvestimentos;
     BottomNavigationView bottomNavigation;
-    ComposeView carteiraChart;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_wallet);
+        setContentView(R.layout.fragment_wallet);
 
-        recyclerInvestimentos = findViewById(R.id.recyclerInvestimentos);
-        carteiraChart = findViewById(R.id.carteiraChart);
-
-        //Trecho do menu
+        inicializarComponente();
+        configurarNavegacao();
+    }
+    private void inicializarComponente(){
         bottomNavigation = findViewById(R.id.bottomNavigation);
-        bottomNavigation.setSelectedItemId(R.id.nav_wallet);
-
-        bottomNavigation.setOnItemSelectedListener(item -> {
-            if(item.getItemId() == R.id.nav_home){
-                Intent it = new Intent(WalletActivity.this, Dashboard.class);
-                startActivity(it);
-                return true;
-            }
-            if(item.getItemId() == R.id.nav_wallet){
-                Intent it = new Intent(WalletActivity.this, WalletActivity.class);
-                startActivity(it);
-                return true;
-            }
-            if(item.getItemId() == R.id.nav_aporte){
-                Intent it = new Intent(WalletActivity.this, Aportes.class);
-                startActivity(it);
-                return true;
-            }
-            return false;
-        });
-
-        recyclerInvestimentos.setLayoutManager(new LinearLayoutManager(this));
-
-        ApiService api = RetrofitClient.getClient().create(ApiService.class);
-        api.getQuote("ITUB4,VALE3,PETR4,PETR4,PETR4").enqueue(new Callback<QuoteResponse>() {
-            @Override
-            public void onResponse(Call<QuoteResponse> call, Response<QuoteResponse> response) {
-
-                if(response.isSuccessful() && response.body() != null){
-                    List<QuoteData> quotes = new ArrayList<>();
-                    for(QuoteResult result : response.body().getResults()){
-                        quotes.add(result.getData());
-                    }
-
-                    QuoteAdapter adapter = new QuoteAdapter(quotes);
-
-                    recyclerInvestimentos.setAdapter(adapter);
-                }
-                else{
-                    Toast.makeText(WalletActivity.this, "Erro: " + response.code(), Toast.LENGTH_LONG).show();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<QuoteResponse> call, Throwable t) {
-
-                Log.e("API", "Erro completo", t);
-                Toast.makeText(WalletActivity.this, "Erro: " + t.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        });
-
-        carregarGraficoPizza();
     }
 
-    public void carregarGraficoPizza(){
-        ArrayList<Number> valores = new ArrayList<>();
+    private void configurarNavegacao(){
+        bottomNavigation.setOnItemSelectedListener(item -> {
+            Fragment fragmentSelecionado;
 
-        valores.add(933.0);
-        valores.add(422.0);
-        valores.add(1380.0);
-        valores.add(600.0);
-        valores.add(1450.0);
+            int itemId = item.getItemId();
 
-        ChartHelper.configurarGraficoPizza(
-                carteiraChart,
-                valores
-        );
+            if(itemId == R.id.nav_wallet){
+                fragmentSelecionado = new WalletFragment();
+            }
+            else if(itemId == R.id.nav_home){
+                fragmentSelecionado = new DashboardFragment();
+            }
+            else if(itemId == R.id.nav_aporte){
+                fragmentSelecionado = new AportesFragment();
+            }
+            else {
+                return false;
+            }
+
+            trocarFragment(fragmentSelecionado);
+            return true;
+        });
+    }
+    private void trocarFragment(Fragment fragment){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frameContent, fragment)
+                .commit();
     }
 }
