e414cf2475d7e5fff64bac01bcc01eb8764b9778
MuriloGoms <murilogcordeiro08@gmail.com>
2026-09-23 15:58:55 -0300

    style: fiz a tela do histórico

---
+++ app/src/main/java/com/example/accurancymobileapp/fragments/HistoricFragment.java
@@ -0,0 +1,47 @@
+package com.example.accurancymobileapp.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.accurancymobileapp.R;
+
+public class HistoricFragment extends Fragment {
+
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public HistoricFragment() {
+        // Required empty public constructor
+    }
+
+    public static HistoricFragment newInstance(String param1, String param2) {
+        HistoricFragment fragment = new HistoricFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_historic, container, false);
+    }
+}
